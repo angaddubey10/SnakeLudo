843f6473461f17b1637a275524d77d975e26f5a1
Angad Dubey <angaddubey@microsoft.com>
2022-10-09 22:16:40 +0530

    Final Make

--- src/main/java/com/example/ludosnake/GameBoard.java
+++ src/main/java/com/example/ludosnake/GameBoard.java
@@ -12,6 +12,32 @@ public class GameBoard {
     static ArrayList<Pair<Integer,Integer>>positionCoordinates;
     static ArrayList<Integer>snakeLadderPosition;
 
+    public GameBoard(){
+        populateSnakeLadderPosition();
+        populatePositionCoordinate();
+    }
+
+    public int getXValue(int piecePosition){
+        if(piecePosition<=100 && piecePosition>=1){
+            return positionCoordinates.get(piecePosition).getKey();
+        }
+        else return positionCoordinates.get(1).getKey();
+    }
+
+    public int getYValue(int piecePosition){
+        if(piecePosition<=100 && piecePosition>=1){
+            return positionCoordinates.get(piecePosition).getValue();
+        }
+        else return positionCoordinates.get(1).getValue();
+    }
+
+    public int playerPositionAtSnakeOrLadder(int piecePosition){
+        if(piecePosition != snakeLadderPosition.get(piecePosition)){
+            return snakeLadderPosition.get(piecePosition);
+        }
+        else return -1;
+    }
+
    static private void populatePositionCoordinate(){
         positionCoordinates = new ArrayList<>();
         positionCoordinates.add(new Pair<Integer,Integer>(20,380));
@@ -29,16 +55,36 @@ public class GameBoard {
             }
         }
 
-        for (int i = 0; i <positionCoordinates.size(); i++) {
-            System.out.println(i + " x: " + positionCoordinates.get(i).getKey() + " y: " + positionCoordinates.get(i).getValue());
+//        for (int i = 0; i <positionCoordinates.size(); i++) {
+//            System.out.println(i + " x: " + positionCoordinates.get(i).getKey() + " y: " + positionCoordinates.get(i).getValue());
+//        }
+    }
+
+    private void populateSnakeLadderPosition(){
+       snakeLadderPosition = new ArrayList<>();
+        for (int i = 0; i < 101; i++) {
+            snakeLadderPosition.add(i);
         }
-
-
+        snakeLadderPosition.set(4,25);
+        snakeLadderPosition.set(13,46);
+        snakeLadderPosition.set(27,5);
+        snakeLadderPosition.set(33,49);
+        snakeLadderPosition.set(40,3);
+        snakeLadderPosition.set(42,63);
+        snakeLadderPosition.set(43,18);
+        snakeLadderPosition.set(50,69);
+        snakeLadderPosition.set(54,31);
+        snakeLadderPosition.set(62,81);
+        snakeLadderPosition.set(66,45);
+        snakeLadderPosition.set(74,92);
+        snakeLadderPosition.set(76,58);
+        snakeLadderPosition.set(89,53);
+        snakeLadderPosition.set(99,41);
     }
 
 
-    public static void main(String[] args) {
-        populatePositionCoordinate();
-    }
+//    public static void main(String[] args) {
+//        populatePositionCoordinate();
+//    }
 
 }
--- src/main/java/com/example/ludosnake/LudoSnake.java
+++ src/main/java/com/example/ludosnake/LudoSnake.java
@@ -1,12 +1,17 @@
 package com.example.ludosnake;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +26,14 @@ public class LudoSnake extends Application {
 
     int yLine = 430;
     int xLine = 40;
+
+    int diceValue=1;
+
+    Label randResult;
+    Button gameButton;
+
+    Player playerOne, playerTwo;
+    boolean gameStart = false, playerOneTurn=true, playerTwoTurn=false;
     public Pane createContent(){
         Pane root = new Pane();
         root.setPrefSize(width*tileSize,height*tileSize+80);
@@ -36,18 +49,69 @@ public class LudoSnake extends Application {
 
         }
 
+        playerOne = new Player(tileSize, Color.WHITE);
+        playerTwo = new Player(tileSize-10, Color.BLACK);
+
+
+        randResult = new Label("Game not started");
+        randResult.setTranslateX(150);
+        randResult.setTranslateY(410);
 
         Button player1Button = new Button("Player One");
         player1Button.setTranslateX(10);
         player1Button.setTranslateY(yLine);
+        player1Button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(gameStart){
+                    if(playerOneTurn){
+                        getDiceValue();
+                        randResult.setText( "PlayerOne - " + String.valueOf(diceValue));
+                        //move the player
+                        playerOne.movePlayer(diceValue);
+                        playerOneTurn = false;
+                        playerTwoTurn = true;
+//                        playerOne.playerAtSnakeOrLadder();
+                        gameOver();
 
-        Button player2Button = new Button("Player Tow");
+                    }
+                }
+            }
+        });
+
+        Button player2Button = new Button("Player Two");
         player2Button.setTranslateX(300);
         player2Button.setTranslateY(yLine);
+        player2Button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(gameStart){
+                    if(playerTwoTurn){
+                        getDiceValue();
+                        randResult.setText("PlayerTwo - " + String.valueOf(diceValue));
+                        //move the player
+                        playerTwo.movePlayer(diceValue);
+                        playerOneTurn = true;
+                        playerTwoTurn = false;
+//                        playerTwo.playerAtSnakeOrLadder();
+                        gameOver();
+                    }
+                }
+            }
+        });
 
-        Button gameButton = new Button("Start Game");
+        gameButton = new Button("Start Game");
         gameButton.setTranslateX(150);
         gameButton.setTranslateY(yLine);
+        gameButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                randResult.setText("Started");
+                gameStart = true;
+                gameButton.setText("Game Going");
+
+            }
+        });
 
 
          //img = new Image("C:\\Users\\angaddubey\\IdeaProjects\\LudoSnake\\src\\snakeLadderBoardNO.jpg");
@@ -58,9 +122,26 @@ public class LudoSnake extends Application {
         boardImage.setFitWidth(tileSize*width);
 
 
-        tileGroup.getChildren().addAll(boardImage, player1Button,player2Button,gameButton);
+        tileGroup.getChildren().addAll(boardImage, randResult, playerOne.getGamePiece(), playerTwo.getGamePiece(), player1Button,player2Button,gameButton);
 
         return root;
+    }
+
+    void gameOver(){
+        if(playerOne.getWinningStatus()==true){
+            randResult.setText("Player One Won");
+            gameButton.setText("Start Again");
+            gameStart = false;
+        }
+        else if(playerTwo.getWinningStatus()==true){
+            randResult.setText("Player Two Won");
+            gameButton.setText("Start Again");
+            gameStart = false;
+        }
+    }
+
+    private void getDiceValue(){
+        diceValue = (int)(Math.random()*6+1);
     }
 
     @Override
@@ -70,6 +151,24 @@ public class LudoSnake extends Application {
         stage.setTitle("Ludo Snake");
         stage.setScene(scene);
         stage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                long currentTime =  System.currentTimeMillis();
+                long dt = currentTime -  Player.lastMovementTime;
+//                System.out.println(currentTime + "  " + Player.lastMovementTime + "   " + dt);
+
+                if(dt>1e3){
+                    Player.lastMovementTime = currentTime;
+//                    System.out.println(currentTime + "  " + Player.lastMovementTime);
+//
+                    playerOne.playerAtSnakeOrLadder();
+                    playerTwo.playerAtSnakeOrLadder();
+                }
+            }
+        };
+        timer.start();
     }
 
     public static void main(String[] args) {
